458e8f2f8ce742f01a8c234c25d0ceb64da5bb73
Miro Hrončok <miro@hroncok.cz>
2014-04-24 00:26:42 +0200

    Make it possible to run methods with arguments in templates

---
+++ wsgi/dapi/templatetags/method_call.py
@@ -0,0 +1,25 @@
+'''This was written by Shah Pavel Jamal
+See http://www.sprklab.com/notes/13-passing-arguments-to-functions-in-django-template
+Waiting to get a permission to use it'''
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='call')
+def call_method(obj, method_name):
+    method = getattr(obj, method_name)
+    if obj.__dict__.has_key("__call_arg"):
+        ret = method(*obj.__call_arg)
+        del obj.__call_arg
+        return ret
+    return method()
+
+
+@register.filter
+def args(obj, arg):
+    if not obj.__dict__.has_key("__call_arg"):
+        obj.__call_arg = []
+
+    obj.__call_arg += [arg]
+    return obj
