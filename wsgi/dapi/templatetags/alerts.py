8c36c84f1ae2cc576fb07af8ec23281c143a6dc6
Miro Hrončok <miro@hroncok.cz>
2014-04-24 02:22:31 +0200

    Template filter that converts Django messages tags to Bootstrap alert classes

---
+++ wsgi/dapi/templatetags/alerts.py
@@ -0,0 +1,20 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+
+register = template.Library()
+
+t = {
+    '': 'info',
+    'error': 'danger',
+    'debug': 'danger',
+}
+
+@register.filter(is_safe=True)
+@stringfilter
+def alerts(value):
+    '''Template filter that converts Django messages tags to Bootstrap alert classes'''
+    try:
+        return t[value]
+    except KeyError:
+        return value
